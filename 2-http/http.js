0322ae200bac412ee1a33a2d8ac6c4fbd92555e4
Drew Kullman <agkullman@protonmail.com>
2023-11-01 14:09:59 -0800

    Commit 3 - Bonus 1

--- 2-http/http.js
+++ 2-http/http.js
@@ -8,29 +8,34 @@ const petsPath = '../pets.json';
 
 let pets = undefined;
 
+const petRegExp = /^\/pets\/(\d+)$/; // Match '/pets/:index'
+
 function start() {
     console.log('Server is running');
     importPetsFromDatabase();
 
     const server = http.createServer(function(req, res) {
         const parsedUrl = url.parse(req.url, true);
-        const urlArray = parsedUrl.pathname.split('/').filter(Boolean); // Remove empty elements
 
-        if (req.method === 'GET' && urlArray[0] === 'pets') {
-            if (urlArray.length === 1) {
-                // Handle '/pets' endpoint
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.end(JSON.stringify(pets));
-            } else if (urlArray.length === 2 && !isNaN(urlArray[1])) {
-                // Handle '/pets/:index' endpoint
-                const index = parseInt(urlArray[1]);
+        if (req.method === 'GET' && parsedUrl.pathname === '/pets') {
+            // Handle '/pets' endpoint
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(pets));
+        } else {
+            // Try to match the pet index using the regular expression
+            const match = parsedUrl.pathname.match(petRegExp);
+
+            if (match) {
+                const index = parseInt(match[1]);
 
                 if (index >= 0 && index < pets.length) {
+                    // Handle '/pets/:index' endpoint
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
                     res.end(JSON.stringify(pets[index]));
                 } else {
+                    // Pet not found
                     res.statusCode = 404;
                     res.setHeader('Content-Type', 'text/plain');
                     res.end('Pet not found');
@@ -41,10 +46,6 @@ function start() {
                 res.setHeader('Content-Type', 'text/plain');
                 res.end('Bad Request');
             }
-        } else {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('Not found');
         }
     });
 
