0e81df1f9386624df21a59e80d32f4559af139ff
Drew Kullman <agkullman@protonmail.com>
2023-11-01 14:03:31 -0800

    Commit 2 - Access Endpoints

--- 2-http/http.js
+++ 2-http/http.js
@@ -2,9 +2,9 @@
 
 import fs from 'fs';
 import http from 'http';
+import url from 'url';
 
 const petsPath = '../pets.json';
-import url from 'url'
 
 let pets = undefined;
 
@@ -13,24 +13,41 @@ function start() {
     importPetsFromDatabase();
 
     const server = http.createServer(function(req, res) {
-        const parsedUrl = url.parse(req.url, true)
-        let urlArray = parsedUrl.path.split('/')
-        
-        console.log(urlArray[1])
-        res.end(JSON.stringify(pets))
+        const parsedUrl = url.parse(req.url, true);
+        const urlArray = parsedUrl.pathname.split('/').filter(Boolean); // Remove empty elements
 
+        if (req.method === 'GET' && urlArray[0] === 'pets') {
+            if (urlArray.length === 1) {
+                // Handle '/pets' endpoint
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(pets));
+            } else if (urlArray.length === 2 && !isNaN(urlArray[1])) {
+                // Handle '/pets/:index' endpoint
+                const index = parseInt(urlArray[1]);
 
-        // if (req.method === 'GET' && req.url === '/pets') {
-        //     const petsJSON = JSON.stringify(pets);
-
-        //     res.setHeader('Content-Type', 'application/json');
-        //     res.end(petsJSON);
-        // } else {
-        //     res.statusCode = 404;
-        //     res.setHeader('Content-Type', 'text/plain');
-        //     res.end('Not found');
-        // }
+                if (index >= 0 && index < pets.length) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(pets[index]));
+                } else {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('Pet not found');
+                }
+            } else {
+                // Invalid URL
+                res.statusCode = 400;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Bad Request');
+            }
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Not found');
+        }
     });
+
     const port = process.env.PORT || 8000;
     server.listen(port, function() {
         console.log('Listening on port', port);
@@ -38,7 +55,7 @@ function start() {
 }
 
 const importPetsFromDatabase = () => {
-    const data = fs.readFileSync(petsPath, 'utf8'); // Specify 'utf8' encoding to read the file as a text
+    const data = fs.readFileSync(petsPath, 'utf8');
     pets = JSON.parse(data);
 }
 
