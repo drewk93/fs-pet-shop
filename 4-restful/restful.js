68bb457cdea77989430dda3aed5e8e88d686c8a7
drewk93 <141455844+drewk93@users.noreply.github.com>
2023-11-09 07:23:04 -0900

    Commit 2 - Added line-by-line comments. w/ Mike Mote.

--- 4-restful/restful.js
+++ 4-restful/restful.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'; // Set code to strict mode to prevent errors
 
-
+// nodemon restful.js
 // Core Module
 import fs from 'fs'; // File System Module
 import express from 'express'; // Express Module
@@ -13,128 +13,133 @@ const petsPath = '../pets.json' // define path to pets;
 
 async function importPets() { 
     try {
-        const data = fs.readFileSync(petsPath, 'utf-8'); // grab the pets.json file and read
-        return JSON.parse(data) // convert from JSON 
+        const data = fs.readFileSync(petsPath, 'utf-8'); // grab the pets.json file and utilize FS readfile function to parse it. 
+        return JSON.parse(data) // convert from JSON data
     } catch (err) {
-        console.error(err.message)
+        console.error(err.message) // Log error message to console
         return [];
     }
 }
 //  Routes 
 async function routes(){
-    console.log(`--- SERVER INITIALIZED---`)
+    console.log(`---ROUTES---`) // Console log that routes is executed properly
 
-let pets = await importPets();
+let pets = await importPets(); // Import read functionality
 
 
 // nodemon restful.js
 // Get All
 app.get('/pets', (req, res, next) => {
     try {
-        if (!pets){
-            const error = new Error('Pets not available')
+        if (!pets){ // If there is no pets instance...
+            const error = new Error('Pets not available') // Error message
             error.status = 404; // Not Found
-            throw error;
+            throw error; // throw error to catch
         }
-        res.status(200);
-        res.json(pets)
+        res.status(200); // OK
+        res.json(pets) // Respond with pets as JSON data
     }catch (err) {
-        next(err)
+        next(err) // Send error to error handling middleware
     }
 })
 
 // Get One
 app.get('/pets/:index', (req, res, next) => {
-    const index = parseInt(req.params.index) // index
+    const index = parseInt(req.params.index) // target index in the request path
     try {
-        if (!pets || isNaN(index) || index < 0 || index >= pets.length){
-            const error = new Error ('Pet is not available');
-            error.status = 404; 
-            throw error;
+        if (!pets || isNaN(index) || index < 0 || index >= pets.length){ // If there is no pets instance, index is not a number, index is less than zero, index is greater than pets array.length...
+            const error = new Error ('Pet is not available'); // Error message
+            error.status = 404; // Not found
+            res.set("Content-Type", "text/plain"); // set response content-type to text/plain
+            throw error; // throw error to catch
         }
-        res.status(200)
-        res.json(pets[index])
+        res.status(200) // OK
+        res.json(pets[index]) // Respond with pet at that pets array as JSON data
     } catch (err) {
-        next(err)
+        next(err) // Send error to error handling middleware
     }
 })
 
 // Post One
 app.post('/pets', async (req,res, next) => {
     try {
-        const newPet = req.body;
-        if (!newPet.age || !newPet.kind || !newPet.name){
-            const error = new Error ('Invalid pet data')
-            error.status = 400;
-            throw error;
+        const newPet = req.body; // Define variable newPet to be equal to the request body
+        if (!newPet.age || !newPet.kind || !newPet.name){ // If the values of age, kind and name are not present in the request body...
+            const error = new Error ('Invalid pet data') // Error message
+            error.status = 400; // Invalid request
+            res.set("Content-Type", "text/plain") //  set response content-type to text/plain
+            throw error; // throw error to catch
         }
-        pets.push(newPet)
-        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8');
-        res.status(201)
-        res.json(newPet); 
+        pets.push(newPet) // Push newpet instance to pets array (line 27) 
+        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8'); // Use FS writefile to stringify data to pets.json file
+        res.status(201) // Created
+        res.json(newPet); // Respond with newPet as confirmation of creation
     } catch (err) {
-    next (err)
+    next (err) // Send error to error middleware
     }
 })
 
 // Patch One 
 
 app.patch('/pets/:id', async (req, res, next) => {
-    const index = parseInt(req.params.id)
+    const index = parseInt(req.params.id) // target index in the request path
     try {
-        if (!pets || isNaN(index) || index < 0 || index >= pets.length) {
-            const error = Error('Pet is not available')
-            error.status = 404; 
-            throw error;
+        if (!pets || isNaN(index) || index < 0 || index >= pets.length) { // If there is no pets instance, index is not a number, index is less than zero, index is greater than pets array.length...
+            const error = Error('Pet is not available') // Error message
+            error.status = 404; // Not found
+            throw error; // throw error to catch
         }
-        const updatedFields = req.body;
-        const existingPet = pets[index];
+        const updatedFields = req.body; // define updatedFields variable to equal the request body (object)
+        const existingPet = pets[index]; // target existing pet at pet index
 
-        for (const key in updatedFields) {
-            if (updatedFields.hasOwnProperty(key)) {
-                existingPet[key] = updatedFields[key]
+        for (const key in updatedFields) { // Iterate over updatedFields object
+            if (updatedFields.hasOwnProperty(key)) { // Target key(property)
+                existingPet[key] = updatedFields[key] // Set existing pet key to now equal updated fields key
             }
         }
-        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8') // Use awa
-        res.status(200);
-        res.json(existingPet)
+        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8') // Use FS writefile to stringify data to pets.json file
+        res.status(200); //OK
+        res.json(existingPet) // Respond with existingPet as confirmation of patch
     } catch (err){
-        next(err);
+        next(err); // Send error to error middleware
     }
 })
 
+// Delete One
+
 app.delete('/pets/:id', async(req, res, next) => {
-    const index = parseInt(req.params.id)
+    const index = parseInt(req.params.id) // target index in the request path
     try {
-        if(!pets || isNaN(index) || index < 0 || index >= pets.length) {
-            const error = new Error ('Pet is not available');
+        if(!pets || isNaN(index) || index < 0 || index >= pets.length) {  // If there is no pets instance, index is not a number, index is less than zero, index is greater than pets array.length...
+            const error = new Error ('Pet is not available'); // Error message
             error.status = 404; // Not Found
-            throw error;
+            throw error; // throw error to catch
         }
         // Remove the pet object from the array
-        const deletedPet  = pets.splice(index, 1)[0]
+        const deletedPet  = pets.splice(index, 1)[0] //Use splice method to remove pet from pet.json file
 
         // Write the updated pets data back to the file
-        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8');
+        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8'); // Use FS writefile to stringify data to pets.json file
         res.status(200) // OK
-        res.json(deletedPet) // send JSON deleted pet
+        res.json(deletedPet) // Respond with deleted pet as confirmation of deletion
     } catch (err) {
-        next(err)
+        next(err) // Send error to error middleware
     }
 });
 
-
+// Error Catch-All Handler
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message); // Use the error's status or default to 500
+    console.error(err.stack); // List errors from previous functions
+    res.set("Content-Type", "text/plain"); //  set response content-type to text/plain
+    res.status(err.status || 500) // Internal Server Error
+    res.send(err.message); // Use the error's status or default to 500
 });
 
 // Port Listener
-app.listen(PORT,  () => {
-    console.log(`Listening on Port: ${PORT}`)
+app.listen(PORT,  () => { 
+    console.log(`Listening on Port: ${PORT}`) //Set server to listen to PORT for incoming requests
 } )
-
 }
 
-routes();
+routes(); //Initialize route function
