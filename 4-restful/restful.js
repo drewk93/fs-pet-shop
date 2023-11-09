d2aa7ded3229a87a3b6c8f8198ef511cb5bae72e
drewk93 <141455844+drewk93@users.noreply.github.com>
2023-11-09 10:55:34 -0900

    Commit 3 - DB installed / new routes w/Mike Mote

--- 4-restful/restful.js
+++ 4-restful/restful.js
@@ -2,76 +2,84 @@
 
 // nodemon restful.js
 // Core Module
-import fs from 'fs'; // File System Module
+// import fs from 'fs'; // File System Module
 import express from 'express'; // Express Module
 const app = express(); // Server instance start
 app.use(express.json()); // express module
 const PORT = 3000; // Set port to listen to 
 
-// File Path
-const petsPath = '../pets.json' // define path to pets;
+// Import Postgres
+import pg from 'pg';
+const { Pool } = pg;
 
-async function importPets() { 
-    try {
-        const data = fs.readFileSync(petsPath, 'utf-8'); // grab the pets.json file and utilize FS readfile function to parse it. 
-        return JSON.parse(data) // convert from JSON data
-    } catch (err) {
-        console.error(err.message) // Log error message to console
-        return [];
-    }
-}
+const pool = new Pool ({
+    user: "postgres",
+    host: "localhost",
+    database: "pets",
+    password: "password",
+    port: "5432"
+})
+
+// File Path
+// const petsPath = '../pets.json' // define path to pets;
+
+// async function importPets() { 
+//     try {
+//         const data = fs.readFileSync(petsPath, 'utf-8'); // grab the pets.json file and utilize FS readfile function to parse it. 
+//         return JSON.parse(data) // convert from JSON data
+//     } catch (err) {
+//         console.error(err.message) // Log error message to console
+//         return [];
+//     }
+// }
 //  Routes 
-async function routes(){
+async function routes() {
     console.log(`---ROUTES---`) // Console log that routes is executed properly
 
-let pets = await importPets(); // Import read functionality
+    // let pets = await importPets(); // Import read functionality
 
 
-// nodemon restful.js
-// Get All
-app.get('/pets', (req, res, next) => {
-    try {
-        if (!pets){ // If there is no pets instance...
-            const error = new Error('Pets not available') // Error message
-            error.status = 404; // Not Found
-            throw error; // throw error to catch
+    // nodemon restful.js
+    // Get All
+    app.get('/pets', async (req, res, next) => {
+        try {
+          const result = await pool.query('SELECT * FROM pets');
+          res.status(200).json(result.rows); // Respond with pets as JSON data
+        } catch (err) {
+          next(err); // Send error to error handling middleware
         }
-        res.status(200); // OK
-        res.json(pets) // Respond with pets as JSON data
-    }catch (err) {
-        next(err) // Send error to error handling middleware
-    }
-})
-
-// Get One
-app.get('/pets/:index', (req, res, next) => {
-    const index = parseInt(req.params.index) // target index in the request path
-    try {
-        if (!pets || isNaN(index) || index < 0 || index >= pets.length){ // If there is no pets instance, index is not a number, index is less than zero, index is greater than pets array.length...
-            const error = new Error ('Pet is not available'); // Error message
-            error.status = 404; // Not found
-            res.set("Content-Type", "text/plain"); // set response content-type to text/plain
-            throw error; // throw error to catch
+      });
+      
+      // Get One
+      app.get('/pets/:id', async (req, res, next) => {
+        const id = parseInt(req.params.id);
+        try {
+          const result = await pool.query('SELECT * FROM pets WHERE id = $1', [id]);
+          if (result.rows.length === 0) {
+            res.status(404).send('NOT FOUND'); // 404 for resource not found
+          } else {
+            res.status(200).json(result.rows[0]); // Respond with the first row as JSON
+          }
+        } catch (err) {
+          next(err); // Send error to error handling middleware
         }
-        res.status(200) // OK
-        res.json(pets[index]) // Respond with pet at that pets array as JSON data
-    } catch (err) {
-        next(err) // Send error to error handling middleware
-    }
-})
+      });
+      
 
-// Post One
+
+// // Post One
 app.post('/pets', async (req,res, next) => {
+    const newPet = req.body
     try {
-        const newPet = req.body; // Define variable newPet to be equal to the request body
         if (!newPet.age || !newPet.kind || !newPet.name){ // If the values of age, kind and name are not present in the request body...
             const error = new Error ('Invalid pet data') // Error message
             error.status = 400; // Invalid request
             res.set("Content-Type", "text/plain") //  set response content-type to text/plain
             throw error; // throw error to catch
         }
-        pets.push(newPet) // Push newpet instance to pets array (line 27) 
-        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8'); // Use FS writefile to stringify data to pets.json file
+        const result = await pool.query('INSERT INTO pets(name, age, kind) VALUES($1, $2, $3) RETURNING id',
+        [newPet.name, newPet.age, newPet.kind])
+        newPet.id = result.rows[0].id;
         res.status(201) // Created
         res.json(newPet); // Respond with newPet as confirmation of creation
     } catch (err) {
@@ -79,49 +87,66 @@ app.post('/pets', async (req,res, next) => {
     }
 })
 
-// Patch One 
+// // Patch One 
 
 app.patch('/pets/:id', async (req, res, next) => {
-    const index = parseInt(req.params.id) // target index in the request path
+    const id= parseInt(req.params.id) // target index in the request path
+    
     try {
-        if (!pets || isNaN(index) || index < 0 || index >= pets.length) { // If there is no pets instance, index is not a number, index is less than zero, index is greater than pets array.length...
+        const petResult = await pool.query('SELECT * FROM pets WHERE id = $1', [id]);
+            
+        if (petResult.rowCount === 0) { // If there is no pets instance, index is not a number, index is less than zero, index is greater than pets array.length...
             const error = Error('Pet is not available') // Error message
             error.status = 404; // Not found
             throw error; // throw error to catch
         }
-        const updatedFields = req.body; // define updatedFields variable to equal the request body (object)
-        const existingPet = pets[index]; // target existing pet at pet index
+        const updatedFields = req.body;
+        const existingPet = petResult.rows[0]; // target existing pet at pet index
 
         for (const key in updatedFields) { // Iterate over updatedFields object
             if (updatedFields.hasOwnProperty(key)) { // Target key(property)
                 existingPet[key] = updatedFields[key] // Set existing pet key to now equal updated fields key
             }
         }
-        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8') // Use FS writefile to stringify data to pets.json file
-        res.status(200); //OK
-        res.json(existingPet) // Respond with existingPet as confirmation of patch
+        const updateResult = await pool.query(
+            'UPDATE pets SET name = $1, age = $2, kind = $3 WHERE id = $4',
+            [existingPet.name, existingPet.age, existingPet.kind, id]
+          );
+        
+        if (updateResult.rowCount === 1){
+            res.status(200)
+            res.json(existingPet)
+        } else {
+            const error = new Error('Failed to update pet')
+            error.status = 500;
+            throw error;
+        }
     } catch (err){
         next(err); // Send error to error middleware
     }
 })
 
-// Delete One
+// // Delete One
 
 app.delete('/pets/:id', async(req, res, next) => {
-    const index = parseInt(req.params.id) // target index in the request path
+    const id = parseInt(req.params.id) // target index in the request path
     try {
-        if(!pets || isNaN(index) || index < 0 || index >= pets.length) {  // If there is no pets instance, index is not a number, index is less than zero, index is greater than pets array.length...
+        const petResult = await pool.query('SELECT * FROM pets WHERE id = $1', [id])
+        if(petResult.rowCount === 0) {  
             const error = new Error ('Pet is not available'); // Error message
             error.status = 404; // Not Found
             throw error; // throw error to catch
         }
+        const deleteResult = await pool.query('DELETE FROM pets WHERE id = $1', [id])
         // Remove the pet object from the array
-        const deletedPet  = pets.splice(index, 1)[0] //Use splice method to remove pet from pet.json file
-
-        // Write the updated pets data back to the file
-        await fs.promises.writeFile(petsPath, JSON.stringify(pets, null, 2), 'utf-8'); // Use FS writefile to stringify data to pets.json file
-        res.status(200) // OK
-        res.json(deletedPet) // Respond with deleted pet as confirmation of deletion
+        if (deleteResult.rowCount === 1){
+            res.status(200)
+            res.json({message: 'Pet deleted succesfully'});
+        } else {
+            const error = new Error('Failed to delete pet')
+            error.status = 500;
+            throw error;
+        }
     } catch (err) {
         next(err) // Send error to error middleware
     }
